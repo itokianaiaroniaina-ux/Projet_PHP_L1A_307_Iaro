69b063ace46ac347e777f6c7e99c826376509f5b
Léo <leofear11@gmail.com>
2026-03-12 10:54:31 +0300

    add index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    require_once 'Encapsulation/'
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../bootstrap-5.3.2/dist/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    
+    <h1>Project PHP</h1>
+
+    <!-- Encpasulation -->
+
+</body>
+</html>
